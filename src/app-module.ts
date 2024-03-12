32c26bae2fe744654b1a1a1c30e92fa74954c859
nembaqs <nemrod@apolloglobal.net>
2024-03-13 01:45:35 +0800

    test: initial setup nestjs

---
+++ src/app-module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppResolver } from './app-resolver';
+import { AppService } from './app-service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
+  controllers: [],
+  providers: [AppService, AppResolver],
+})
+export class AppModule {}
